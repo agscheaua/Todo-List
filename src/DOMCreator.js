232e7e8879dc0eb7c3f2c509ee5c2bf3aa92811a
Alex <agscheaua@gmail.com>
2026-04-19 15:06:48 +0300

    i make the logic for the container that will hold the todos, now i need to style him

--- src/DOMCreator.js
+++ src/DOMCreator.js
@@ -11,7 +11,8 @@ function createWebpage() {
   createModalProject();
   createButtonProject(); 
   createProjectBoard();
-  createProjects();
+  createProject();
+  projectsContainer;
 };
  
 // gets all the elements created in the html template documnent;
@@ -29,6 +30,12 @@ const getStaticElements = (function() {
   return {createButton, modalContainer, inputSpace,
     submitModal, cancelModal, sideBar, projectsContainer,
     projectBoardContainer,};
+}) ();
+
+// object that contains all the object dynamically created;
+
+const projectsContainer = (function() {
+  return {};
 }) ();
 
 // modal creation and buttons functionality; 
@@ -63,7 +70,7 @@ function createButtonProject() {
     } 
     else {
       const projectButton = document.createElement("button");
-      projectButton.classList.add("projectButton" + projectsNr);
+      projectButton.classList.add("projectButton" + projectsNr, "projButton");
       projectsNr++;
       getStaticElements.projectsContainer.appendChild(projectButton);
       projectButton.textContent = getStaticElements.inputSpace.value;
@@ -71,37 +78,52 @@ function createButtonProject() {
   });
 };
 
-//
+// function to create an object for each project button created;
 
-function createProjectBoard() {
-  getStaticElements.projectsContainer.addEventListener("click", (eve) => {
-    if (eve.target.classList.containes("hasBoard")) {
-      console.log("no");
+function createProject() {
+  getStaticElements.submitModal.addEventListener("click", () => {
+    if (getStaticElements.projectsContainer.lastElementChild) {
+      const lastChildElem = getStaticElements.projectsContainer.lastElementChild;
+      const project = new ProjectsCreator(lastChildElem.textContent);
+      projectsContainer[`${lastChildElem.textContent}`] = project;
+      projectsContainer[`${lastChildElem.textContent}`].createTodo();
+      console.log(projectsContainer);
     }
-    else {
-    const projectBoard = document.createElement("div");
-    projectBoard.textContent = "Project board of" + eve.target.textContent;
-    getStaticElements.projectBoardContainer.appendChild(projectBoard);
-    eve.target.classList.add("hasBoard");
+    else{
+      console.log("no child"); 
     };
   });
 };
 
+// function to create a board that contains the todos for each project button;
 
-
-
-function createProjects() {
-  getStaticElements.submitModal.addEventListener("click", () => {
-    if (getStaticElements.projectsContainer.lastElementChild) {
-      let lastChildElem = getStaticElements.projectsContainer.lastElementChild;
-      const project = new ProjectsCreator(lastChildElem.textContent);
-      console.log(project);
+function createProjectBoard() {
+  getStaticElements.projectsContainer.addEventListener("click", (eve) => {
+    if (eve.target.classList.contains("hasBoard") || eve.target.nodeName != "BUTTON") {
+      console.log("a board alredy exist");
     }
-    else{
-      console.log("no child"); 
-    }
+    else {
+      eve.target.classList.add("hasBoard");
+
+      const projectBoard = document.createElement("div");
+      getStaticElements.projectBoardContainer.appendChild(projectBoard);
+      projectBoard.classList.add(`board${eve.target.textContent}`, "projectBoard");
+     
+      const projectBoardTitle = document.createElement("div");
+      projectBoard.appendChild(projectBoardTitle);
+      projectBoardTitle.classList.add(`projectBoardTitle${eve.target.textContent}`);
+      projectBoardTitle.textContent = eve.target.textContent;
+
+      const addTodoToBoard = document.createElement("button");
+      projectBoard.appendChild(addTodoToBoard);
+      addTodoToBoard.classList.add(`addTodo${eve.target.textContent}`);
+      addTodoToBoard.textContent = "Add a new todo.";
+    }; 
   });
 };
+
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import {ProjectsCreator} from "./projectsCreator.js";
 import {createWebpage} from "./DOMCreator.js";
 import {getStaticElements} from "./DOMCreator.js";
 
-const project1 = new ProjectsCreator("Project1");
 
-project1.createTodo();
-project1.createTodo();
-console.log(project1); 
+ 
